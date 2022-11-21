df0db31f82a6c30ae428ac818656ad9996fb80ea
UgurcanOruc <ugurcan-oruc@hotmail.com>
2022-11-21 21:54:45 +0300

    babel installed and configurations are made

--- src/app.js
+++ src/app.js
@@ -1,11 +1,7 @@
-import subtract, { square, add } from './utils.js';
 
-console.log("app.js is running???");
-console.log(square(1));
-console.log(add(1, 2));
-console.log(subtract(1, 2));
+import React from 'react';
+import ReactDOM, { render } from 'react-dom';
 
-// import { isAdult, canDrink } from './person';
+const template = <p>deneme</p>;
 
-// console.log(isAdult(15));
-// console.log(canDrink(22));
+ReactDOM.render(template, document.getElementById('app'));
